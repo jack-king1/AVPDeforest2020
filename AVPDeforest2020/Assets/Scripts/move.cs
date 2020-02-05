75d6278bb65785c7b9ff85f6f681c61f4b43d903
King <jack3.king@live.uwe.ac.uk>
2020-02-05 23:41:16 +0000

    Cloud Generator + Static Effects Manager for lerping stuff

---
+++ AVPDeforest2020/Assets/Scripts/move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    float Speed;
+
+    public void Init(float speed)
+    {
+        GetComponent<MeshRenderer>().material.color = new Color(1f, 1f, 1f, 0.0f);
+        Speed = speed;
+    }
+
+    public void Move()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * Speed);
+    }
+}
