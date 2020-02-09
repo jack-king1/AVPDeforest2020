c29e33f54ab9aafa817ec62b71e8b5bf0ba5b581
King <jack3.king@live.uwe.ac.uk>
2020-02-09 02:17:57 +0000

    Added Timer and Butterflies

---
+++ AVPDeforest2020/Assets/PlayAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animation animation;
+    public Timer t;
+    public bool playAnim;
+
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+        t = new Timer( );
+        t.Init(4f, 8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(t.Count() && playAnim)
+        {
+            Debug.Log("Animation Playing");
+            animation.Play();
+            playAnim = false;
+        }
+        else if(playAnim == false && t.timer > 0 )
+        {
+            playAnim = true;
+        }
+    }
+}
