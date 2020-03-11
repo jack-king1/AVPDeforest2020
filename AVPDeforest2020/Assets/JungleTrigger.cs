d0cd895e9eb7431eb0980d3bcdb1ed813a825dce
JackLynham <JackLynham>
2020-03-11 20:36:15 +0000

    Fixed Narration with other audio

--- AVPDeforest2020/Assets/JungleTrigger.cs
+++ AVPDeforest2020/Assets/JungleTrigger.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class JungleTrigger : MonoBehaviour
 {
     public bool startSFX = false;
+    Narration narration;
+
+
+    private void Awake()
+    {
+        narration = GetComponent<Narration>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("MainCamera"))
@@ -14,6 +22,7 @@ public class JungleTrigger : MonoBehaviour
             AudioManager.instance.Play("Jungle");
             AudioManager.instance.Play("Cicada");
             AudioManager.instance.Play("Alt");
+            narration.StartCoroutine(narration. PlayScene1());
 
 
         }
