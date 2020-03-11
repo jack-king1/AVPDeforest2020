fd62ad1c087479fc08fcb84ad58260066b4ad761
JackLynham <JackLynham>
2020-03-11 19:25:46 +0000

    Added transition of Audio From Intro to Scene 1, Added narration in

---
+++ AVPDeforest2020/Assets/JungleTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JungleTrigger : MonoBehaviour
+{
+    public bool startSFX = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            startSFX = true;
+            AudioManager.instance.Stop("Intro");
+            AudioManager.instance.Play("Jungle");
+            AudioManager.instance.Play("Cicada");
+            AudioManager.instance.Play("Alt");
+
+
+        }
+    }
+}
+
--- AVPDeforest2020/Assets/NextScene.cs
+++ AVPDeforest2020/Assets/NextScene.cs
@@ -8,17 +8,16 @@ public class NextScene : MonoBehaviour
    public bool startSFX = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("MainCamera"))
         {
             Debug.Log("Scene Trigger Hit");
-            SceneManagerKing.instance.FadeToLevel();
             startSFX = true;
             AudioManager.instance.Stop("Intro");
 
             AudioManager.instance.Play("Jungle");
             AudioManager.instance.Play("Cicada");
             AudioManager.instance.Play("Alt");
-
+          
 
         }
     }
