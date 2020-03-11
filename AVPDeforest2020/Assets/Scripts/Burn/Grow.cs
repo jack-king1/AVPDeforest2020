e492c89eea83aa67cb66ee6680d23172b3f6f1fa
lbondi7 <lewis2.bond@live.uwe.ac.uk>
2020-03-11 17:32:39 +0000

    Hope tree grows

--- AVPDeforest2020/Assets/Scripts/Burn/Grow.cs
+++ AVPDeforest2020/Assets/Scripts/Burn/Grow.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Grow : MonoBehaviour
 {
 
-    bool grow = true;
+    [SerializeField] int level = 0;
+    [SerializeField] bool grow = false;
     [SerializeField][Range(-10.0f, 10.0f)] float growRate = 2.0f;
     float health = 0.0f;
     Color startColour = new Color();
@@ -14,14 +15,24 @@ public class Grow : MonoBehaviour
     Vector3 startSize = new Vector3();
     Vector3 endSize = new Vector3();
 
+    public bool Growing { get => grow; set => grow = value; }
+    public int Level { get => level; set => level = value; }
+
+    bool setChildren = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        var col = gameObject.GetComponent<MeshRenderer>().material.color;
 
-        startColour = Color.black;
+    }
+
+    void Awake()
+    {
+        var col = gameObject.GetComponent<MeshRenderer>().material.color;
         endColour = col;
         col = Color.black;
+        col.a = 0.0f;
+        startColour = col;
         gameObject.GetComponent<MeshRenderer>().material.color = col;
         startSize = new Vector3(0.0f, 0.0f, 0.0f);
         endSize = transform.localScale;
@@ -43,6 +54,21 @@ public class Grow : MonoBehaviour
             scale = Vector3.Lerp(startSize, endSize, health / 100.0f);
             transform.localScale = scale;
             health += Time.deltaTime * growRate;
+
+            if(!setChildren && health > 50.0f)
+            {
+                List<Transform> children = new List<Transform>();
+                children.AddRange(GetComponentsInChildren<Transform>());
+
+                foreach(var child in children)
+                {
+                     child.gameObject.GetComponent<Grow>().Growing = true;
+                }
+                //child.gameObject.GetComponent<Grow>().Growing = true;
+                setChildren = true;
+            }
+
+
         }
     }
 }
