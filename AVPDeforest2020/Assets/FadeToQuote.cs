f81f4bc8b9fa82236b63359b65749c52840b6cc1
Jack <jack3.king@live.uwe.ac.uk>
2020-03-11 21:12:37 +0000

    Fading

---
+++ AVPDeforest2020/Assets/FadeToQuote.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeToQuote : MonoBehaviour
+{
+    public GameObject VRCam;
+    public GameObject quote;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("FadeToQuote");
+        FadeOut();
+        quote.SetActive(true);
+    }
+
+    public void FadeOut()
+    {
+        Debug.Log("Fadeing Out!");
+        VRCam.GetComponent<OVRScreenFade>().FadeOut(3.5f);
+    }
+
+    public void FadeIn()
+    {
+        Debug.Log("Fadeing In!");
+        VRCam.GetComponent<OVRScreenFade>().FadeIn(3.5f);
+    }
+}
